eda8278172592a6d1fdd8149764ebad068c64984
lpatgo <leahpatgorski@gmail.com>
2024-04-26 14:53:23 -0400

    friday style exp

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,52 +39,57 @@ export default function Page() {
             <ul className="flex space-x-4">
               <li className="bg-offwhite hover:bg-highlight p-2 pt-3  rounded-bl-md rounded-br-md font-sansfont"><a href="#" className="text-cool">Probst Bets</a></li>
               <li className="bg-offwhite hover:bg-highlight p-2 pt-3  rounded-bl-md rounded-br-md  font-sansfont"><a href="#" className="text-cool">Circa Survivor History</a></li>
-              <li className="bg-offwhite hover:bg-highlight p-2 pt-3  rounded-bl-md rounded-br-md  font-sansfont"><a href="https://www.youtube.com/results?search_query=chillest+dogs" className="text-cool">Find the chillest dogs</a></li>
+              <li className="bg-offwhite hover:bg-highlight p-2 pt-3  rounded-bl-md rounded-br-md  font-sansfont"><a href="https://www.youtube.com/results?search_query=chillest+dogs" className="text-cool" target='_blank'>Find the chillest dogs</a></li>
             </ul>
           </nav>
         </header>
 
-        <div className="clear-both w-full h-4/5 mx-0 mt-26 mb-0.5 p-1">
+        <div className="clear-both flex justify-center w-full h-4/5 mx-auto mt-26 mb-3 px-2 py-4 shadow-sm">
 
           {/* Main content area with tabs */}
 
-          <Tabs defaultValue="current" className="w-full">
+          <Tabs defaultValue="current" className="w-5/6  bg-white py-10 opacity-95  rounded-lg shadow-md">
             <TabsList>
-              <TabsTrigger value="current">CURRENT</TabsTrigger>
-              <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
-              <TabsTrigger value="leaderboard">Leaderboard</TabsTrigger>
-              <TabsTrigger value="community">Community</TabsTrigger>
+              <TabsTrigger value="current" className="bg-offwhite hover:bg-highlight p-2 pt-3" >Current</TabsTrigger>
+              <TabsTrigger value="upcoming" className="bg-offwhite hover:bg-highlight p-2 pt-3">Upcoming</TabsTrigger>
+              <TabsTrigger value="leaderboard" className="bg-offwhite hover:bg-highlight p-2 pt-3">Leaderboard</TabsTrigger>
+              <TabsTrigger value="community" className="bg-offwhite hover:bg-highlight p-2 pt-3">Community</TabsTrigger>
             </TabsList>
             <TabsContent value="current">
-              <p className="font-sansfont leading-8 text-m mb-20">
+              <p className="font-sansfont leading-8 text-m mt-10 mb-20">
                 How does it feel<br />
                 To treat me like you do?<br />
                 When you've laid your hands upon me<br />
-                And told me who you are
+                And told me who you are<br/><br/>
+                <Button>Previous</Button>
               </p>
             </TabsContent>
             <TabsContent value="upcoming">
-              <p className="font-sansfont leading-8 text-m mb-20">
+              <p className="font-sansfont leading-8 text-m mt-10 mb-20">
                 I thought I was mistaken<br />
                 I thought I heard your words<br />
                 Tell me how do I feel<br />
-                Tell me now, how do I feel
+                Tell me now, how do I feel <br/><br/>
+                <Button>So... let's play!</Button>
               </p>
             </TabsContent>
             <TabsContent value="leaderboard">
-              <p className="font-sansfont leading-8 text-m mb-20">
+              <p className="font-sansfont leading-8 text-m mt-10 mb-20">
                 Those who came before me<br />
                 Lived through their vocations<br />
                 From the past until completion<br />
-                They'll turn away no more
+                They'll turn away no more<br/><br/>
+                <Button>Learn</Button>
               </p>
             </TabsContent>
             <TabsContent value="community">
-              <p className="font-sansfont leading-8 text-m mb-20">
+              <p className="font-sansfont leading-8 text-m mt-10 mb-20">
                 I see a ship in the harbor<br />
                 I can and shall obey<br />
                 But if it wasn't for your misfortune<br />
-                I'd be a heavenly person today
+                I'd be a heavenly person today <br/><br/>
+                <Button>Join</Button>
+
               </p>
             </TabsContent>
           </Tabs>
