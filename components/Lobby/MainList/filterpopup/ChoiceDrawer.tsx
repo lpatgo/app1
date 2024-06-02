850a419d77786b01c5a653bd16f23107c89c7510
g <gabep@hey.com>
2024-06-02 10:58:56 -0700

    chocietoggles

--- components/Lobby/MainList/filterpopup/ChoiceDrawer.tsx
+++ components/Lobby/MainList/filterpopup/ChoiceDrawer.tsx
@@ -11,14 +11,14 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
-import { PlusIcon } from "lucide-react"
- 
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 interface ChoiceDrawerProps {
     label: string
 }
  
 export function ChoiceDrawer({ label }: ChoiceDrawerProps) {
   const [goal, setGoal] = useState(350)
+  const [selectedChoicesIds, setSelectedChoicesIds] = useState<string[]>([])
   const nflAbbreviations = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC", "LAC", "LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG", "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS"]
  
   function onClick(adjustment: number) {
@@ -36,15 +36,16 @@ export function ChoiceDrawer({ label }: ChoiceDrawerProps) {
             <DrawerTitle>{label}</DrawerTitle>
             <DrawerDescription>Select</DrawerDescription>
           </DrawerHeader>
-          <div className="p-4 pb-0">
-            <div className="flex flex-wrap gap-2">
-                {nflAbbreviations.map((abbr) => (
-                    <Button key={abbr} onClick={() => onClick(1)}>{abbr}</Button>
-                ))}
-                </div>
-           
-
-           </div>
+          <ToggleGroup value={selectedChoicesIds} type='multiple' onValueChange={(values) => setSelectedChoicesIds(values)} >
+              
+                    <div className='flex flex-wrap gap-2'>
+                        {nflAbbreviations.map((choice, index) => (
+                            <ToggleGroupItem key={index} value={choice}>
+                                {choice}
+                            </ToggleGroupItem>
+                        ))}
+                    </div>
+                </ToggleGroup>
 
 
           <DrawerFooter>
